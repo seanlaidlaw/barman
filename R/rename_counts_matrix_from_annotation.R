087af8cfea9887960c17d352f8ead23a977a05f7
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-01-20 01:25:46 +0000

    add rename columns and files functions

---
+++ R/rename_counts_matrix_from_annotation.R
@@ -0,0 +1,36 @@
+#' @title barman
+#'
+#' @description Renames the counts matrix column names from a provided annotation table
+#'
+#' @param counts_matrix the featurecounts matrix
+#' @param annotation_table annotation table containing at least one column with the featurecounts column names, and another column we want to use to rename the counts matrix.
+#' @param dna_cell_id the columnname in the annotation_table, we want to use as the new featurecounts column names
+#' @param rna_cell_id (optional) the columnname in the annotation_table containing the current featurecounts columnnames
+#'
+#' @return a renamed counts matrix
+#'
+#' @export
+
+rename_counts_matrix_from_annotation <- function(counts_matrix, annotation_table, dna_cell_id, rna_cell_id="") {
+	# we need to match the header of the featurecounts matrix with a Cell_ID of some sort, from the annotation table so that we can have the same ID for our DNA and RNA data.
+	# this renames the counts matrix headers to the specified Cell_ID column from the annotation.
+	sample_colnames = colnames(counts_matrix)[!colnames(counts_matrix) %in% c("Geneid","Chr","Start","End","Strand","Length")]
+
+	# if rna_cell_id not given then determine from annotation_table
+	if (rna_cell_id == "") {
+		rna_cell_id = ""
+		for (annotation_colname in colnames(annotation_table)) {
+			if (all(sample_colnames %in% annotation_table[[annotation_colname]])) {
+				rna_cell_id = annotation_colname
+			}
+		}
+		if (rna_cell_id == "") {
+			print("Error: Couldn't automatically determine which column of annotation corresponded to colnames of counts matrix. please manually specify with the rna_cell_id argument")
+			return(NA)
+		}
+	}
+
+	new_sample_colnames = annotation_table[[dna_cell_id]][match(sample_colnames, annotation_table[[rna_cell_id]])]
+	colnames(counts_matrix) = append(c("Geneid","Chr","Start","End","Strand","Length"), new_sample_colnames)
+	return(counts_matrix)
+}
---
+++ R/rename_segment_files_from_annotation.R
@@ -0,0 +1,46 @@
+#' @title barman
+#'
+#' @description Renames a folder of segmentation files to remove the bloat from filenames, leaving only the cell id and a ".txt" suffix
+#'
+#' @param segment_files_dir the folder of files to rename
+#' @param annotation_table annotation table containing a column of unique cell IDs that can be used to differentiate each segment file
+#' @param dna_cell_id the columnname in the annotation_table of the column containg the unique cell IDs
+#'
+#' @return Nothing
+#'
+#' @export
+
+rename_segment_files_from_annotation <- function(segment_files_dir, annotation_table, dna_cell_id) {
+	# do a first pass to check regex is specifc to just one file per dna_id and that those files exist
+	nb_of_detected_files = 0
+	for (dna_id in annotation_table[[dna_cell_id]]) {
+		# look for any file in folder containing dna_cell_id and containing ".[a-z]{3}" (e.g. .txt or .tsv or .csv)
+		cell_id_file_finder_regex = paste0(".*?",dna_id,".*?\\....")
+		segment_files = list.files(segment_files_dir, pattern=cell_id_file_finder_regex)
+
+		if (length(segment_files) > 1) {
+			print(paste0("Error: regex of '", cell_id_file_finder_regex, "' is not specific enough to only find 1 file for this dna_id: '", dna_id, "'."))
+			return(NA)
+		} else if (length(segment_files) == 1) {
+			nb_of_detected_files = nb_of_detected_files + 1
+		}
+	}
+
+	if (nb_of_detected_files == 0) {
+		print(paste0("Error: No files with '", dna_id, "' regex were detected in folder :'", segment_files_dir, "'."))
+		return(NA)
+	}
+
+	# if didnt fail due to non-specific regex or absent files, then rename files to dna_id.txt
+	for (dna_id in annotation_table[[dna_cell_id]]) {
+		# look for any file in folder containing dna_cell_id and containing ".[a-z]{3}" (e.g. .txt or .tsv or .csv)
+		cell_id_file_finder_regex = paste0(".*?",dna_id,".*?\\....")
+		segment_files = list.files(segment_files_dir, pattern=cell_id_file_finder_regex)
+
+		if (length(segment_files) == 1) {
+			print(segment_files)
+			file.rename(from = paste0(segment_files_dir,"/",segment_files), to = paste0(segment_files_dir,"/",dna_id, ".txt"))
+		}
+	}
+
+}
