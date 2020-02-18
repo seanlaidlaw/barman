28448e635912fd116354e0846f87480d0ce749b2
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-02-18 20:29:03 +0000

    make bulk_get_expression_by_segment multithreaded

--- R/bulk_get_expression_by_segment.R
+++ R/bulk_get_expression_by_segment.R
@@ -5,11 +5,12 @@
 #' @param counts_matrix a featureCounts matrix
 #' @param dna_segments_dir folder of CNV segment files from scCNV or converted from ASCAT with ascat2sccnvseg()
 #' @param output_dir an output directory to store RNAsegment files
+#' @param threads number of threads to run concurrently (defaults to number of avaliable cores minus 1)
 #'
 #' @return Nothing
 #'
 #' @export
-bulk_get_expression_by_segment = function(counts_matrix, dna_segments_dir, output_dir) {
+bulk_get_expression_by_segment = function(counts_matrix, dna_segments_dir, output_dir, threads) {
 
 	# get list of samplenames from counts_matrix header
 	cell_sample_names = colnames(counts_matrix)[!(colnames(counts_matrix) %in% c("Geneid","Chr","Start","End","Strand","Length"))]
@@ -28,7 +29,7 @@ bulk_get_expression_by_segment = function(counts_matrix, dna_segments_dir, outpu
 		}
 	}
 	if (!anyfile) {
-		print("Error: No match was found between filenames in dna_segments_dir and column names in counts_matrix")
+		print("Error: No match was found between filenames in dna_segments_dir and column names in counts_matrix, verify that cell_ids are the same in column names of counts matrix and in DNA segment files.")
 		return(NA)
 	}
 
@@ -36,18 +37,53 @@ bulk_get_expression_by_segment = function(counts_matrix, dna_segments_dir, outpu
 	# create output directory
 	dir.create(file.path(output_dir), showWarnings = FALSE)
 
-	# for each sample_name find a dna_segment file that includes sample_name in filename
+	# tell user how many RNA to DNA matches were found
+	present_in_dna = c()
 	for (sample_name in cell_sample_names) {
 		if (any(grepl(paste0(".*?",sample_name, ".*"), list.files(dna_segments_dir)))) {
-
-			dna_seg_filename = list.files(dna_segments_dir)[grepl(paste0(".*?",sample_name, ".*"), list.files(dna_segments_dir))]
-			dna_seg = read.table(paste0(dna_segments_dir,"/",dna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
-
-			# get RNA expression for each segment of the segmented DNA
-			rna_seg = get_expression_by_segment(cell_id = sample_name, counts_matrix = counts_matrix, segment_file = dna_seg)
-
-			# write to output folder for further analysis later
-			write.table(rna_seg, paste0(output_dir, "/RNAsegments_", sample_name, "_from_DNA_segments.tsv"), sep = "\t", row.names = F, col.names = T)
+			present_in_dna = append(present_in_dna, values = sample_name, after = length(present_in_dna))
 		}
 	}
+	if (length(present_in_dna) == length(cell_sample_names)) {
+		print("Found all RNA names in DNA segments folder")
+	} else {
+		print(paste0("Found ", length(present_in_dna), " DNA samples corresponding to ", length(cell_sample_names), " RNA names"))
+	}
+
+
+	# Define a function where the heavy lifting happens, that can be easily inserted into a foreach loop to parallelise
+	parallel_seg_counter = function(sample_name) {
+		print(sample_name)
+
+		# read in DNA segment file
+		dna_seg_filename = list.files(dna_segments_dir)[grepl(paste0(".*?",sample_name, ".*"), list.files(dna_segments_dir))]
+		dna_seg = read.table(paste0(dna_segments_dir,"/",dna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
+
+		# get RNA expression for each segment of the segmented DNA
+		rna_seg = get_expression_by_segment(cell_id = sample_name, counts_matrix = counts_matrix, segment_file = dna_seg)
+
+		# write to output folder for further analysis later
+		write.table(rna_seg, paste0(output_dir, "RNAsegments_", sample_name, "_from_DNA_segments.tsv"), sep = "\t", row.names = F, col.names = T)
+	}
+
+
+	#setup parallel backend to use many processors
+	total_cores = parallel::detectCores()
+
+	# if threads option not given then run all detected but 1
+	if (missing(threads)) {
+		threads = total_cores[1]-1
+	}
+	cores_cluster <- parallel::makeCluster(threads) #not to overload your computer
+	doParallel::registerDoParallel(cores_cluster)
+
+	library(doParallel)
+	foreach::foreach(i=1:(length(present_in_dna))) %dopar% {
+		library(barman)
+
+		parallel_seg_counter(present_in_dna[i])
+	}
+
+	parallel::stopCluster(cores_cluster)
+
 }
