4b2b537ac7081ce60353638ea95b4cd1da92bb9c
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-02-24 10:47:58 +0000

    fix zscore bug by removing control groups and having transformation applied from all provided cell names

--- R/zscore_transform.R
+++ R/zscore_transform.R
@@ -1,22 +1,21 @@
 #' @title barman
 #'
-#' @description apply a zscore transformation to a list of columns based on average and SD of a control list of column names. Zscore should only be applied on counts matrix containing values that follow a normal distribution.
+#' @description apply a zscore transformation to a list of columns based on average and SD of a column names provided by all_cells_to_normalise arg. Zscore should only be applied on counts matrix containing values that follow a normal distribution.
 #'
 #' @param all_cells_to_normalise list of column names that we want to zscore transform
 #' @param counts_matrix counts matrix with normally distrubted data
-#' @param control_groups (optional) list of column names corresponding to samples we want to use for mean and SD calculations, in case we only want to calculate average expression from a control group.
 #'
-#' @return Nothing
+#' @return zscore transformed counts matrix
 #'
 #' @export
-zscore_transform <- function(all_cells_to_normalise, control_groups, counts_matrix) {
+zscore_transform <- function(all_cells_to_normalise, counts_matrix) {
 
 	# sanity check given arguments
-	if (!any(control_groups %in% all_cells_to_normalise)) {
-		print("Error: none of the control group cells are in the all_cells_to_normalise list.")
-		return(NA)
-	}
-	if (!any(all_cells_to_normalise %in% counts_matrix)) {
+	# if (!any(control_groups %in% all_cells_to_normalise)) {
+	# 	print("Error: none of the control group cells are in the all_cells_to_normalise list.")
+	# 	return(NA)
+	# }
+	if (!any(all_cells_to_normalise %in% colnames(counts_matrix))) {
 		print("Error: none of the cells in all_cells_to_normalise list are counts_matrix column names.")
 		return(NA)
 	}
@@ -31,7 +30,7 @@ zscore_transform <- function(all_cells_to_normalise, control_groups, counts_matr
 	# calculate Z scores
 	zscore_counts$mean <- rowMeans(as.matrix(zscore_counts[, all_cells_to_normalise]))
 	zscore_counts$stdD <- matrixStats::rowSds(as.matrix(zscore_counts[, all_cells_to_normalise]))
-	zscore_counts <- as.data.frame(t(apply(zscore_counts[, c("mean", "stdD", control_groups)], 1, function(x) ((x - x[["mean"]]) / x[["stdD"]]))))
+	zscore_counts <- as.data.frame(t(apply(zscore_counts[, c("mean", "stdD", all_cells_to_normalise)], 1, function(x) ((x - x[["mean"]]) / x[["stdD"]]))))
 	zscore_counts$mean <- NULL
 	zscore_counts$stdD <- NULL
 
