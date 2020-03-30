95eaa2e15a02a6a5995171ab177bc6bfd79f80aa
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-03-30 13:27:42 +0100

    update description with Github links

--- build_package.sh
+++ build_package.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-R -e 'setwd("/Users/sl31/Documents/barman")'
-R -e 'devtools::document();devtools::build()'
-R -e 'pkgdown::build_site(preview=F)'
+R -e 'setwd("/Users/sl31/Documents/barman");devtools::document();devtools::build()'
 R -e 'devtools::install()'
+R -e 'setwd("/Users/sl31/Documents/barman");pkgdown::deploy_to_branch(pkg=".")'
