c49f79594ca9c2d64ba7706e6fba3c636ef0fce0
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-04-06 14:46:52 +0100

    rewrite logic in bulk_G_T_ plot fct to work with DNA, RNA or both

--- R/bulk_G_T_chr_plots.R
+++ R/bulk_G_T_chr_plots.R
@@ -31,6 +31,7 @@ bulk_G_T_chr_plots <- function(dna_segments_dir, rna_segments_dir, output_dir=".
 
   # get sample_ids from folders provided
 	if ((!missing(dna_segments_dir)) & (!missing(rna_segments_dir))) {
+		# if we have both DNA and RNA segments, the RNA are more aggressivly filtered so use that list
 		segments_dir = rna_segments_dir
 	} else if (!missing(rna_segments_dir)) {
 		segments_dir = rna_segments_dir
@@ -42,39 +43,38 @@ bulk_G_T_chr_plots <- function(dna_segments_dir, rna_segments_dir, output_dir=".
   for (file in list.files(segments_dir)) {
   	sample_ids <- append(sample_ids, gsub("(_from_DNA_segments.tsv)|(-PCF-kmin.*copynumber.refLOCUS.NA.txt)", "", gsub("(SEGMENTSlogR.GCcorrected-M30-)|(RNAsegments_)", "", file)), length(sample_ids))
   }
-  print(paste0("Generating plots for samples : ", unlist(sample_ids)))
+  print(paste0("Generating plots for samples : ", paste(names(sample_ids),sample_ids,sep=" ",collapse=";")))
 
 
 
 
-  # Define a function where the heavy lifting happens, that can be easily inserted into a foreach loop to parallelise
-  parallel_seg_counter <- function(sample_name, run_DNA=TRUE, run_RNA=TRUE) {
-    if (run_DNA) {
-      # read in DNA segment file
-      dna_seg_filename <- list.files(dna_segments_dir)[grepl(paste0(".*?", sample_name, ".*"), list.files(dna_segments_dir))]
-      dna_seg <- read.table(paste0(dna_segments_dir, "/", dna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
-    }
+	# Define a function where the heavy lifting happens, that can be easily inserted into a foreach loop to parallelise
+	parallel_seg_counter <- function(sample_name, run_DNA=TRUE, run_RNA=TRUE) {
 
-    if (run_RNA) {
-      # read in RNA segment file
-      rna_seg_filename <- list.files(rna_segments_dir)[grepl(paste0(".*?", sample_name, ".*"), list.files(rna_segments_dir))]
-      rna_seg <- read.table(paste0(rna_segments_dir, "/", rna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
-    }
+		plot_title <- paste0(sample_name)
 
+		# with both we can plot both
+		if (run_DNA & run_RNA) {
+			dna_seg_filename <- list.files(dna_segments_dir)[grepl(paste0(".*?", sample_name, ".*"), list.files(dna_segments_dir))]
+			dna_seg <- read.table(paste0(dna_segments_dir, "/", dna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
+			rna_seg_filename <- list.files(rna_segments_dir)[grepl(paste0(".*?", sample_name, ".*"), list.files(rna_segments_dir))]
+			rna_seg <- read.table(paste0(rna_segments_dir, "/", rna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
 
-    plot_title <- paste0(sample_name)
+			plt <- G_T_chr_plot(cnv_data = dna_seg, exp_data = rna_seg,output_dir=output_dir, title = plot_title, save = T)
 
+		} else if (run_DNA) {
+			# read in DNA segment file
+			dna_seg_filename <- list.files(dna_segments_dir)[grepl(paste0(".*?", sample_name, ".*"), list.files(dna_segments_dir))]
+			dna_seg <- read.table(paste0(dna_segments_dir, "/", dna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
+			plt <- G_T_chr_plot(cnv_data = dna_seg,output_dir=output_dir, title = plot_title, save = T)
+		} else if (run_RNA) {
+			# read in RNA segment file
+			rna_seg_filename <- list.files(rna_segments_dir)[grepl(paste0(".*?", sample_name, ".*"), list.files(rna_segments_dir))]
+			rna_seg <- read.table(paste0(rna_segments_dir, "/", rna_seg_filename), header = T, sep = "\t", stringsAsFactors = F)
+			plt <- G_T_chr_plot(exp_data = rna_seg,output_dir=output_dir, title = plot_title, save = T)
+		}
 
-    # if ((!missing(rna_segments_dir)) & (!skip_DNA)) {
-    #   plt <- G_T_chr_plot(cnv_data = dna_seg, exp_data = rna_seg, title = plot_title, save = T)
-    # } else if (missing(rna_segments_dir)) {
-    #   plt <- G_T_chr_plot(cnv_data = dna_seg, title = plot_title, save = T)
-    # } else if (missing(dna_segments_dir)) {
-    #   plt <- G_T_chr_plot(exp_data = rna_seg, title = plot_title, save = T)
-    # }
-    #
-    #
-    # return(plt)
+	 return(plt)
   }
 
 
@@ -97,6 +97,7 @@ bulk_G_T_chr_plots <- function(dna_segments_dir, rna_segments_dir, output_dir=".
 
   print(run_RNA)
 
+  library(doParallel)
   foreach::foreach(i = 1:(length(sample_ids))) %dopar% {
     library(barman)
 
