eeae43d5cec623ddd35d27b4f8d71ec05119c630
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-05-26 03:35:07 +0100

    update plot_rna_clusters to accept matrices instead of SCE and remove complexity

--- R/plot_rna_clusters.R
+++ R/plot_rna_clusters.R
@@ -2,21 +2,36 @@
 #'
 #' @description plots UMAP of a filtered and normalized SingleCellExperiement object
 #'
-#' @param normalised_SCE_object a filtered and normalized SingleCellExperiement, like the type returned from running filter_and_normalise_scRNA() with the return_sce=T parameter.
-#' @param umap_vars a vector of colnames from annotation table, to manually specifiy colour_by and size_by values respectively
-#' @param regress_out a vector of colnames from annotation table, to regress out of dataset before plotting
+#' @param normalised_counts_matrix a filtered, normalized, and log transformed counts matrix
+#' @param annotation_table a table of cell annotations, with  rows corresponding to colnames in normalised_counts_matrix
+#' @param umap_vars a vector of 2 colnames from annotation table, to manually specifiy the plot's colour and size values respectively
 #'
 #' @return ggplot2 object of UMAP
 #'
 #' @export
 
 
-plot_rna_clusters <- function(normalised_SCE_object, regress_out=FALSE, umap_vars=FALSE) {
+plot_rna_clusters <- function(normalised_counts_matrix, annotation_table, umap_vars=FALSE) {
 
-	if (regress_out != FALSE) {
-		normalised_SCE_object <- Seurat::ScaleData(object = Seurat::as.Seurat(normalised_SCE_object), vars.to.regress = "log10_total_features_by_counts")
-		normalised_SCE_object <- Seurat::as.SingleCellExperiment(normalised_SCE_object)
-	}
+	# trim metadata columns from counts matrix
+	trimmed_counts_matrix = normalised_counts_matrix
+	rownames(trimmed_counts_matrix) = trimmed_counts_matrix$Geneid
+	trimmed_counts_matrix$Geneid = NULL
+	trimmed_counts_matrix$Chr = NULL
+	trimmed_counts_matrix$Start = NULL
+	trimmed_counts_matrix$End = NULL
+	trimmed_counts_matrix$Strand = NULL
+	trimmed_counts_matrix$Length = NULL
+
+	# trim annotation table to be same as counts matrix
+	annotation_table = annotation_table[rownames(annotation_table) %in% colnames(trimmed_counts_matrix),]
+
+	# construct SingleCellExperiment object from normalized tables
+	normalised_SCE_object <- SingleCellExperiment::SingleCellExperiment(
+		assays = list(logcounts = as.matrix(trimmed_counts_matrix)),
+		colData = annotation_table
+	)
+
 
 	print(scater::plotExplanatoryPCs(normalised_SCE_object, npcs_to_plot = 5))
 
